5dceaab7324efa97beeac0f80f463c772b233b99
Ashbrook <RAshbrook@patriottrans.com>
2020-11-18 11:22:06 -0500

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'gitlog1',
+    buildEnd() {
+        //git command to run
+        const g = "git log -1 --pretty=format:%H%n%ae%n%ad";
+        //where to pipe it to
+        const o = ".\\public\\build\\commit.txt";
+        //cmd to run g, pipe to o
+        const c = `${g} > ${o}`
+        //child process ref
+        const { exec } = require('child_process');
+        //exec c and show status
+        exec(c,()=>console.log(`\n\ngitlog1 ran:\n\n\t${c}\n\n`))
+    }
+}
